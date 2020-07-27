db9a564f0d1d2fe162512ec591dca56d30717443
david-gribouille <kevin@martin.best>
2020-07-27 19:00:32 +0200

    fix 100 vh

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./src/components/Main/Main";
 
+// First render
+const vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+// All resize
+window.addEventListener("resize", () => {
+  const vh2 = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh2}px`);
+});
+
 ReactDOM.render(<Main />, document.getElementById("root"));
